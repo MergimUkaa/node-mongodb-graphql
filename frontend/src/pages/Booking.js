b0a5250aa5077e740cbbbc243bf6d9448268c712
mergimuka-psih <mergimuka1@gmail.com>
2019-07-15 00:15:17 +0200

    adding react SPA, adding react-router-dom library, and creating routing for auth,events and booking

---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+class BookingPage extends Component{
+    render() {
+        return (
+            <h1>Hello from booking page</h1>
+        );
+    }
+}
+
+export default BookingPage;
