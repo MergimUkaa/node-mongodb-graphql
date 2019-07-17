706f682e039a91fbe7859c8f78621d992bff6012
mergimuka-psih <mergimuka1@gmail.com>
2019-07-17 09:48:21 +0200

    adding & styling navbar to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingPage from "./pages/Booking";
-
+import MainNavigation from "./components/Navigation/MainNavigation";
 class App extends Component {
   render() {
     return (
      <BrowserRouter>
-         <Switch>
-           <Redirect from='/' to='/auth' exact/>
-           <Route path='/auth' component={AuthPage}/>
-           <Route path='/events' component={EventsPage}/>
-           <Route path='/booking' component={BookingPage}/>
-         </Switch>
+             <MainNavigation/>
+         <main className="main-content">
+             <Switch>
+                 <Redirect from='/' to='/auth' exact/>
+                 <Route path='/auth' component={AuthPage}/>
+                 <Route path='/events' component={EventsPage}/>
+                 <Route path='/booking' component={BookingPage}/>
+             </Switch>
+         </main>
      </BrowserRouter>
     );
   }
